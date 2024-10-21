af62361cf50683757cf5f5bac41bbbe186e6fc1c
josue <futermaxh@gmail.com>
2024-10-21 10:02:02 -0600

    ejercicio 7 y 8

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,1 +1,14 @@
 '''
+Ejercicio 7:
+  Programa que reciba una cantidad de minutos y los transforme en horas y minutos correspondientes
+Entradas:
+  minutos: int
+Salidas:
+  Horas: int
+  Minutos: int
+'''
+minutos = input("Ingresa los minutos: ")
+minutos = int(minutos)
+horas = minutos//60
+minutos = minutos % 60
+print("Las horas y minutos son: ",horas , ":", minutos)
---
+++ ejercicio8.py
@@ -0,0 +1,15 @@
+'''
+Ejercicio 8:
+  Programa que calcule el sueldo de un vendedor por obtener el 10% extra de comisión de venta en un mes por tres ventas
+Entradas:
+  Sueldo: int
+  Variable1: int
+  Variable2: int
+  Variable3: int
+Salidas:
+  Dinero ganado por mes
+'''
+variable1 = input("Ingresar venta: ")
+variable1 = int(variable1)
+variable2 = input("Ingresar venta: ")
+variable2 = int(variable2)
